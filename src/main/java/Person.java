ea2799afb3260fa16e4b69d68b240fe8cb90b372
fredrik <fredrik.a.holtet@gmail.com>
2021-10-18 14:37:18 +0200

    pusher uferdig kode

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,7 @@
 public class Person {
     private Long id;
+    private String firstName;
+    private String lastName;
 
     public Long getId() {
         return id;
@@ -7,5 +9,21 @@ public class Person {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
